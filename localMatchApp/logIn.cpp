be405f27ad35738dc098f205801c4fb4406db850
Roberto Chaves <robchaves@aligntech.com>
2018-12-04 14:06:50 -0600

    Change position of the branch and fix code that was indicated

---
+++ localMatchApp/logIn.cpp
@@ -0,0 +1,31 @@
+#include "logIn.h"
+
+using namespace std;
+
+logIn::logIn()
+{
+    //Connect to database.
+}
+
+logIn::~logIn()
+{
+    //disconnect to database.
+}
+
+int logIn::signUp(const string user, const string password)
+{
+  //Send information to database.
+  return 0;
+}
+
+bool logIn::isEqualPass(const string pass, const string rePass)
+{
+  //Compare strings and return TRUE for equal.
+  return true;
+}
+
+int logIn::entry(const string user, const string password)
+{
+  //search the user and compare password
+  return 0;
+}
