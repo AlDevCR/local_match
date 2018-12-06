671c65c4b232cba6ea64f08e0b25d136c630b31d
Marcelino Solano <masolano@aligntech.com>
2018-12-06 11:19:29 -0600

    Create new project with the user interface
    Create project with the login interface, signup interface and main window of the system of Local Match

---
+++ localMatchApp/headers/signup.h
@@ -0,0 +1,33 @@
+#ifndef SIGNUP_H
+#define SIGNUP_H
+
+#include <QDialog>
+
+/*!
+ * The class of user interface type is created to be
+ * able to develop the signup window
+*/
+namespace Ui {
+class SignUp;
+}
+
+class SignUp : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SignUp(QWidget *i_parent = nullptr);
+    ~SignUp();
+
+/*!
+ * The functions of button actions are declared so
+ * they can be used later
+*/
+private slots:
+    void onNewUserButtonClicked();
+
+private:
+    Ui::SignUp *ui;
+};
+
+#endif // SIGNUP_H
