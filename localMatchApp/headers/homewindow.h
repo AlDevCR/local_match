9cb3febe13b6133e8c1904c1aee7fcfe71fcd8f5
Roberto Chaves <robchaves@aligntech.com>
2018-12-11 11:01:34 -0600

    Add clang format

--- localMatchApp/headers/homewindow.h
+++ localMatchApp/headers/homewindow.h
@@ -6,21 +6,20 @@
 /*!
  * The class of user interface type is created to
  * be able to develop the main screen
-*/
+ */
 namespace Ui {
 class HomeWindow;
 }
 
-class HomeWindow : public QDialog
-{
-    Q_OBJECT
+class HomeWindow : public QDialog {
+  Q_OBJECT
 
-public:
-    explicit HomeWindow(QWidget *i_parent = nullptr);
-    ~HomeWindow();
+ public:
+  explicit HomeWindow ( QWidget *i_parent = nullptr );
+  ~HomeWindow ( );
 
-private:
-    Ui::HomeWindow *ui;
+ private:
+  Ui::HomeWindow *ui;
 };
 
 #endif // HOMEWINDOW_H
